c055c803ac42057cdae0e0da0ad6bacbd6697701
skoogh00 <skoogh@student.chalmers.se>
2023-05-28 10:59:26 +0200

    luasnips start

--- lua/plugins/luasnip/all.lua
+++ lua/plugins/luasnip/all.lua
@@ -5,9 +5,19 @@ local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
 
 return {
-  s({ trig = "greek", snippetType = "autosnippet" }, {
-    t({ "s({ trig = '", i(1), "', snippetType='autosnippet' }," }, { "{t(\\", i(2), ")}," }, { ")," }),
-  }),
+  s(
+    { trig = "snip", name = "snippet" },
+    fmta(
+      [[
+        s({ trig = "<>", name = "<>" }, {
+          t("<>"),
+        }), ]],
+      { i(1), i(2), i(3) }
+    )
+  ),
 }
--- lua/plugins/luasnip/tex.lua
+++ lua/plugins/luasnip/tex.lua
@@ -5,12 +5,67 @@ local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
 
 return {
-  s({ trig = ",a", snippetType = "autosnippet" }, {
+
+  -- Greek letters
+  s({ trig = ",a", name = "alpha", snippetType = "autosnippet" }, {
     t("\\alpha"),
   }),
-  s({ trig = ",b", snippetType = "autosnippet" }, {
+  s({ trig = ",b", name = "beta" }, {
     t("\\beta"),
   }),
+  s({ trig = ",g", name = "gamma" }, {
+    t("\\gamma"),
+  }),
+  s({ trig = ",p", name = "psi" }, {
+    t("\\psi"),
+  }),
+  s({ trig = ",o", name = "omega" }, {
+    t("\\omega"),
+  }),
+  s({ trig = ",t", name = "theta" }, {
+    t("\\theta"),
+  }),
+
+  -- Math
+  s(
+    { trig = "ff", name = "frac" },
+    fmt("\\frac{<>}{<>}", {
+      i(1),
+      i(2),
+    }, { delimiters = "<>" })
+  ),
+
+  s(
+    { trig = "eq", name = "equation" },
+    fmta(
+      [[
+        \begin{equation}
+          <>
+        \end{equation}
+      ]],
+      { i(1) }
+    )
+  ),
+
+  -- General
+  s(
+    { trig = "env", name = "Environment", snippetType = "autosnippet" },
+    fmta(
+      [[
+      \begin{<>}
+          <>
+      \end{<>}
+    ]],
+      {
+        i(1),
+        i(2),
+        rep(1),
+      }
+    )
+  ),
 }
