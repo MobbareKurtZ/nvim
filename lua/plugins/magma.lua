cccd181667e8809882bf2cc156e0855f00c64d6f
skoogh00 <skoogh@student.chalmers.se>
2023-10-21 09:29:20 +0200

    small fixes

--- lua/plugins/magma.lua
+++ lua/plugins/magma.lua
@@ -1,7 +1,7 @@
 vim.cmd([[
+let g:magma_image_provider = "ueberzugpp"
 let g:magma_automatically_open_output = v:false
 ]])
--- let g:magma_image_provider = "ueberzug"
 
 return {
   "dccsillag/magma-nvim",
