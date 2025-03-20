d9e5ba4b690374cccad2b2799b5ecaa361325c49
Hannes Skoog <hannes.skoog@bitaddict.se>
2025-03-20 08:30:56 +0100

    codesnap

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,12 @@
+return {
+  "mistricky/codesnap.nvim",
+  build = "make",
+  opts = {
+    mac_window_bar = false,
+    watermark = "",
+    has_line_number = true,
+    show_workspace = true,
+    has_breadcrumbs = true,
+    code_font_family = "JetBrainsMonoNL Nerd Font Mono",
+  },
+}
