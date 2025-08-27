506da04d1028babe934a97f197c1af6ae9629dcd
MobbareKurtZ <hannes.skoog@gmail.com>
2025-08-27 14:08:29 +0200

    Window keymaps'

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -23,8 +23,12 @@ vim.keymap.set("n", "<leader>fj", ":Telescope file_browser<CR>", opts)
 vim.keymap.set("n", "<leader>fk", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", opts)
 
 -- UI
+local diag = require("tiny-inline-diagnostic")
 vim.keymap.set("n", "<leader>uw", "<cmd>set wrap!<CR>", { desc = "Set text wrap" })
 vim.keymap.set("n", "<leader>ug", "<cmd>GitBlameToggle<CR>", { desc = "Toggle git blame" })
+vim.keymap.set("n", "<leader>ud", function()
+	require("tiny-inline-diagnostic").toggle()
+end, { desc = "Toggle Inline Diagnostics" })
 vim.keymap.set("n", "<leader>l", ":Lazy<CR>", opts)
 
 -- Code
@@ -34,3 +38,21 @@ vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code A
 -- Terminal
 vim.keymap.set("t", "<ESC>", "<C-\\><C-n>")
 vim.keymap.set("t", "<C-n>", "<C-\\>")
+
+-- Windows
+vim.keymap.set("n", "<leader>wd", "<C-W>c", { desc = "Delete window" })
+vim.keymap.set("n", "<leader>ws", "<C-W>s", { desc = "Split window below" })
+vim.keymap.set("n", "<leader>wv", "<C-W>v", { desc = "Split window right" })
+vim.keymap.set("n", "<C-h>", "<C-W>h", { desc = "Move window left" })
+vim.keymap.set("n", "<C-j>", "<C-W>j", { desc = "Move window down" })
+vim.keymap.set("n", "<C-k>", "<C-W>k", { desc = "Move window up" })
+vim.keymap.set("n", "<C-l>", "<C-W>l", { desc = "Move window right" })
+
+vim.keymap.set("n", "<C-Right>", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
+vim.keymap.set("n", "<C-Left>", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
+vim.keymap.set("n", "<C-Up>", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
+vim.keymap.set("n", "<C-Down>", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
+
+-- moving
+vim.keymap.set("n", "å", "{")
+vim.keymap.set("n", "ä", "}")
